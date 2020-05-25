32a34121ddc35aeb2d7570b2d8e83a75ddd54ba1
bo li <ms10823268@outlook.com>
2020-05-25 18:19:14 +0800

    s1

---
+++ WebApi2020/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi2020.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Route("[action]")]
+        public string S1()
+        {
+            return "S1";
+        }
+    }
+}
